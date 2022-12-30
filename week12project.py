7e7434fd69a48ff94918ecc678467fb5696c6434
Brent Collins <brentcollins012@yahoo.com>
2022-12-30 17:58:48 +0000

    updated black snake repo

--- week12project.py
+++ week12project.py
@@ -10,9 +10,13 @@ aws_services.append('CloudFormation')
 aws_services.append('Kubernetes')
 aws_services.insert(3, 'S3')
 aws_services.insert(4, 'Lambda')
+aws_services.insert(5, 'Cloud9')
 
 
 # 3) Print the list and length of the list
 print(aws_services)
+print(len(aws_services))
+
+
 
 
