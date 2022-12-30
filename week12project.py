591afc3fb18eaf40f3eba2cb5c306cdb5d193647
Brent Collins <brentcollins012@yahoo.com>
2022-12-30 17:27:11 +0000

    Creating a List

---
+++ week12project.py
@@ -0,0 +1,18 @@
+# 1) Create an empty list of AWS services
+
+
+aws_services = []
+
+# 2) Populate the list using append and/or insert
+
+aws_services.append('Python')
+aws_services.append('CloudFormation')
+aws_services.append('Kubernetes')
+aws_services.insert(3, 'S3')
+aws_services.insert(4, 'Lambda')
+
+
+# 3) Print the list and length of the list
+print(aws_services)
+
+
